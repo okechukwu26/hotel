b936f1f170a4f7b4fef4a5ddfe42cd8cfd92e0bf
mac <mac@macs-MacBook-Air-2.local>
2023-08-13 23:33:27 +0100

    render

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+//pages
+import Home from "./pages/Home";
+import Rooms from "./pages/Rooms";
+import Error from "./pages/Error";
+import SingleRooms from "./pages/SingleRooms";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <Routes>
+        {/* Provide the `element` prop for the root path */}
+        <Route path="/" element={<Home />} />
+        <Route path="/rooms" element={<Rooms />} />
+        <Route path="/rooms/:slug" element={<SingleRooms />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </>
   );
 }
 
